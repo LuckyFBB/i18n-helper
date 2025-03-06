3cdda19d21582998d94d4927559a0ff985a681fa
LuckyFBB <976060700qq.com>
2025-03-06 19:00:40 +0800

    feat: 🎸 implement extraction of Chinese text to i18n
    ✅ Closes: #1

--- src/extension.ts
+++ src/extension.ts
@@ -2,33 +2,114 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import { getLocaleValue, loadLocales } from './utils';
+import {
+    containsChinese,
+    generateLocaleKey,
+    getAstContext,
+    getLocaleValue,
+    getProjectConfig,
+    getSortKey,
+    loadLocales,
+    updateLocaleContent,
+} from './utils';
+
+const lodash = require('lodash-es');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-    loadLocales();
+export async function activate(context: vscode.ExtensionContext) {
+    const locales = await loadLocales();
+    const extractCommand = vscode.commands.registerCommand(
+        'i18n-helper.extract',
+        async (params) => {
+            const { text, range } = params || {};
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                return;
+            }
+
+            if (!text) {
+                vscode.window.showWarningMessage('请先选择需要提取的中文文本');
+                return;
+            }
+            const fileName = editor.document.fileName;
+            const fileKey = generateLocaleKey(fileName);
+            const projectConfig = getProjectConfig();
+            const defaultLocale = locales?.[projectConfig.sourceLocale];
+            const currObj = lodash.get(defaultLocale, fileKey);
+            const sortKey = getSortKey(1, currObj);
+            lodash.set(currObj, sortKey, text);
+            updateLocaleContent(defaultLocale);
+
+            // 获取当前文件内容
+            const fileContent = editor.document.getText();
+            // 分析 AST 上下文
+            const contextType = await getAstContext(fileContent, range);
+            const isString = contextType.includes('string');
+            // 创建编辑器范围
+            const editorRange = new vscode.Range(
+                new vscode.Position(
+                    range[0].line,
+                    isString ? range[0].character - 1 : range[0].character,
+                ),
+                new vscode.Position(
+                    range[1].line,
+                    isString ? range[1].character + 1 : range[1].character,
+                ),
+            );
+
+            editor.edit((editBuilder) => {
+                editBuilder.replace(
+                    editorRange,
+                    contextType.includes('jsx')
+                        ? `{I18N.${fileKey}.${sortKey}}`
+                        : contextType.includes('template')
+                          ? `\${I18N.${fileKey}.${sortKey}}`
+                          : `I18N.${fileKey}.${sortKey}`,
+                );
+            });
+        },
+    );
     const provider = vscode.languages.registerHoverProvider(
         ['typescript', 'javascript', 'typescriptreact', 'javascriptreact'],
         {
             provideHover(document, position, token) {
-                const range = document.getWordRangeAtPosition(
-                    position,
-                    /I18N(?:\.[a-zA-Z0-9_$]+)+/,
-                );
+                const range = document.getWordRangeAtPosition(position);
                 if (!range) {
                     return;
                 }
                 const word = document.getText(range);
-                const translation = getLocaleValue(word.replace('I18N.', ''));
-                if (translation) {
-                    return new vscode.Hover(translation);
+                if (word.indexOf('I18N.') === -1) {
+                    const translation = getLocaleValue(
+                        word.replace('I18N.', ''),
+                    );
+                    if (translation) {
+                        return new vscode.Hover(translation);
+                    }
+                }
+                if (containsChinese(word)) {
+                    const actions = [
+                        `[提取到 i18n](command:i18n-helper.extract?${encodeURIComponent(
+                            JSON.stringify([
+                                {
+                                    text: word,
+                                    range,
+                                },
+                            ]),
+                        )})`,
+                    ].join(' | ');
+                    const commandUri = new vscode.MarkdownString(actions);
+                    commandUri.isTrusted = true;
+                    return new vscode.Hover([
+                        new vscode.MarkdownString(word),
+                        commandUri,
+                    ]);
                 }
             },
         },
     );
 
-    context.subscriptions.push(provider);
+    context.subscriptions.push(provider, extractCommand);
 }
 
 // This method is called when your extension is deactivated
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,20 +3,29 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { parse } from '@babel/parser';
 import traverse from '@babel/traverse';
-import { isIdentifier, isObjectExpression } from '@babel/types';
+import * as babelTypes from '@babel/types';
+import generate from '@babel/generator';
+import slash from 'slash2';
 const lodash = require('lodash-es');
 
 let locales: Record<string, any> = {};
 
 // i18n.config.json 默认值
 const DEFAULT_CONFIG = {
-    localeDir: 'locales',
+    localeDir: './locales',
     extractDir: './',
     importStatement: 'import I18N from @/utils/i18n',
     excludeFile: [],
-    excludeDir: ['node_modules'],
+    excludeDir: ['node_modules', 'locales'],
     type: 'ts',
+    sourceLocale: 'zh-CN',
 };
+
+export enum LOCALE_FILE_TYPES {
+    TS = 'ts',
+    JS = 'js',
+    JSON = 'json',
+}
 
 type IConfig = typeof DEFAULT_CONFIG;
 
@@ -35,17 +44,17 @@ const getSubdirectories = async (directoryPath: string): Promise<string[]> => {
         );
 };
 
-const getProjectConfig = async (): Promise<IConfig> => {
+export const getProjectConfig = (): IConfig => {
     const configFile = path.join(
         `${vscode.workspace.rootPath}/i18n.config.json`,
     );
     if (!fs.existsSync(configFile)) {
-        return Promise.reject('当前项目尚未配置 i18n.config.json');
+        throw new Error('尚未找到 i18n.config.json 文件');
     }
-    return Promise.resolve({
+    return {
         ...DEFAULT_CONFIG,
         ...JSON.parse(fs.readFileSync(configFile, 'utf-8')),
-    });
+    };
 };
 
 /**
@@ -53,7 +62,7 @@ const getProjectConfig = async (): Promise<IConfig> => {
  */
 export const loadLocales = async () => {
     try {
-        const config = await getProjectConfig();
+        const config = getProjectConfig();
         const localeDir = path.join(
             vscode.workspace.rootPath || '',
             config.localeDir,
@@ -74,6 +83,7 @@ export const loadLocales = async () => {
         results.forEach(({ lang, res }) => {
             locales[lang] = res;
         });
+        return locales;
     } catch (error) {
         vscode.window.showErrorMessage(error as string);
     }
@@ -97,10 +107,10 @@ const getExportedObject = (filePath: string) => {
     traverse(ast, {
         ExportDefaultDeclaration(path) {
             const declaration = path.node.declaration;
-            if (isIdentifier(declaration)) {
+            if (babelTypes.isIdentifier(declaration)) {
                 exportIdentifier = declaration.name;
             }
-            if (isObjectExpression(declaration)) {
+            if (babelTypes.isObjectExpression(declaration)) {
                 exportData = eval(
                     `(${code.slice(declaration.start ?? 0, declaration.end ?? 0)})`,
                 );
@@ -151,5 +161,198 @@ export const getLocaleValue = (path: string): string | undefined => {
         return text;
     } else {
         return undefined;
+    }
+};
+
+/**
+ * 检测是否包含中文字符
+ */
+export const containsChinese = (text: string): boolean => {
+    return /[\u4e00-\u9fa5]/.test(text);
+};
+
+export const generateLocaleKey = (filePath: string) => {
+    const projectConfig = getProjectConfig();
+
+    const basePath = path.resolve(
+        vscode.workspace.rootPath || '',
+        projectConfig.extractDir,
+    );
+
+    const relativePath = path.relative(basePath, filePath);
+
+    const names = slash(relativePath).split('/');
+    const fileName = lodash.last(names) as any;
+    let fileKey = fileName.split('.').slice(0, -1).join('.');
+    const dir = names.slice(0, -1).join('.');
+    if (dir) fileKey = names.slice(0, -1).concat(fileKey).join('.');
+    return fileKey.replace(/-/g, '_');
+};
+
+export const getSortKey = (n: number, extractMap = {}): string => {
+    let label = '';
+    let num = n;
+    while (num > 0) {
+        num--;
+        label = String.fromCharCode((num % 26) + 65) + label;
+        num = Math.floor(num / 26);
+    }
+    const key = `${label}`;
+    if (lodash.get(extractMap, key)) {
+        return getSortKey(n + 1, extractMap);
+    }
+    return key;
+};
+
+export const objectToAst = (
+    obj: Record<string, any> | string,
+): babelTypes.ObjectExpression => {
+    const data = typeof obj === 'string' ? JSON.parse(obj) : obj;
+    const properties = Object.entries(data).map(([key, value]) => {
+        let valueNode: babelTypes.Expression;
+        if (value && typeof value === 'object') {
+            valueNode = objectToAst(value);
+        } else if (typeof value === 'string') {
+            valueNode = babelTypes.stringLiteral(value);
+        } else {
+            valueNode = babelTypes.valueToNode(value);
+        }
+        return babelTypes.objectProperty(
+            babelTypes.stringLiteral(key),
+            valueNode,
+        );
+    });
+
+    return babelTypes.objectExpression(properties);
+};
+
+/**
+ * 创建或更新国际化资源文件
+ * @param {string} content - 要写入的国际化内容
+ */
+export const updateLocaleContent = (
+    content: Record<string, any>,
+    filePath?: string,
+) => {
+    const { localeDir, type, sourceLocale } = getProjectConfig();
+    const targetFilename =
+        filePath ||
+        path.join(
+            vscode.workspace.rootPath || '',
+            localeDir,
+            `${sourceLocale}/index.${type}`,
+        );
+    const directory = path.dirname(targetFilename);
+
+    if (!fs.existsSync(directory)) {
+        fs.mkdirSync(directory, { recursive: true });
+    }
+
+    if (
+        [LOCALE_FILE_TYPES.TS, LOCALE_FILE_TYPES.JS].includes(
+            type as LOCALE_FILE_TYPES,
+        )
+    ) {
+        const newAst = objectToAst(content);
+
+        const sourceCode = fs.readFileSync(targetFilename, 'utf-8');
+        const ast = parse(sourceCode, {
+            sourceType: 'module',
+            plugins: type === LOCALE_FILE_TYPES.TS ? ['typescript'] : [],
+        });
+
+        let exportedIdentifier: string | null = null;
+
+        traverse(ast, {
+            ExportDefaultDeclaration(path) {
+                const declaration = path.node.declaration;
+                if (babelTypes.isIdentifier(declaration)) {
+                    exportedIdentifier = declaration.name;
+                }
+                if (babelTypes.isObjectExpression(declaration)) {
+                    path.node.declaration = newAst;
+                }
+                path.stop();
+            },
+        });
+
+        if (exportedIdentifier) {
+            traverse(ast, {
+                VariableDeclarator(path) {
+                    if (
+                        babelTypes.isIdentifier(path.node.id) &&
+                        path.node.id.name === exportedIdentifier
+                    ) {
+                        path.node.init = newAst;
+                        path.stop();
+                    }
+                },
+            });
+        }
+
+        const { code } = generate(ast, {
+            jsescOption: {
+                minimal: true,
+            },
+        });
+        fs.writeFileSync(targetFilename, code, 'utf8');
+        return;
+    }
+
+    fs.writeFileSync(targetFilename, JSON.stringify(content, null, 4), 'utf8');
+};
+
+/**
+ * 分析代码的 AST 上下文
+ * @param code 代码内容
+ * @param range 文本范围
+ * @returns 上下文类型
+ */
+export const getAstContext = async (
+    code: string,
+    range: any,
+): Promise<string[]> => {
+    try {
+        const ast = parse(code, {
+            sourceType: 'module',
+            plugins: ['jsx', 'typescript', 'classProperties'],
+        });
+
+        const contextTypes = new Set<string>(); // 默认为普通文本
+
+        traverse(ast, {
+            enter(path) {
+                const { node } = path;
+                if (!node.loc) {
+                    return path.stop();
+                }
+                // 检查节点位置是否包含目标范围
+                if (
+                    node.loc.start.line <= range[0].line + 1 &&
+                    node.loc.end.line >= range[1].line + 1
+                ) {
+                    if (babelTypes.isStringLiteral(node)) {
+                        contextTypes.add('string');
+                    }
+                    if (babelTypes.isTemplateLiteral(node)) {
+                        contextTypes.add('template');
+                    }
+                    if (
+                        babelTypes.isJSXElement(node) ||
+                        babelTypes.isJSXAttribute(node)
+                    ) {
+                        contextTypes.add('jsx');
+                    }
+                    if (babelTypes.isObjectProperty(node)) {
+                        contextTypes.add('property');
+                    }
+                }
+            },
+        });
+
+        return Array.from(contextTypes);
+    } catch (error) {
+        console.error('AST 分析错误:', error);
+        return []; // 出错时返回默认类型
     }
 };
