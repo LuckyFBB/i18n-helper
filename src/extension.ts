08842e7d2910ce5a8f0b60b48f69d55ad0dd548a
霜序 <976060700@qq.com>
2025-03-12 18:48:34 +0800

    feat: 🎸 support extract chinese by QuickFix (#2)
    * feat: 🎸 support extract chinese by QuickFix
    
    * feat: 🎸 use I18NProvider
    
    ✅ Closes: #1
    ---------
    
    Co-authored-by: LuckyFBB <976060700qq.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
-import { getLocaleValue, loadLocales } from './utils';
+import I18NProvider from './i18nProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-    loadLocales();
-    const provider = vscode.languages.registerHoverProvider(
-        ['typescript', 'javascript', 'typescriptreact', 'javascriptreact'],
-        {
-            provideHover(document, position, token) {
-                const range = document.getWordRangeAtPosition(
-                    position,
-                    /I18N(?:\.[a-zA-Z0-9_$]+)+/,
-                );
-                if (!range) {
-                    return;
-                }
-                const word = document.getText(range);
-                const translation = getLocaleValue(word.replace('I18N.', ''));
-                if (translation) {
-                    return new vscode.Hover(translation);
-                }
-            },
-        },
-    );
-
-    context.subscriptions.push(provider);
+export async function activate(context: vscode.ExtensionContext) {
+    new I18NProvider(context);
 }
 
 // This method is called when your extension is deactivated
